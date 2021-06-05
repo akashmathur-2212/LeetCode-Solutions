ff99bee037fc46c24536ccaf37e878cec29e0020
Akash Mathur <63149422+akashmathur-2212@users.noreply.github.com>
2021-06-05 17:30:36 +0530

    Update second-highest-salary.sql

--- MySQL/second-highest-salary.sql
+++ MySQL/second-highest-salary.sql
@@ -1,6 +1,20 @@
 # Time:  O(n)
 # Space: O(1)
 
+select Max(salary) as SecondHighestSalary
+
+from Employee
+where salary < (select MAX(salary)
+                from Employee); 
+
+#OR
+select salary as SecondHighestSalary
+from Employee
+order by salary DESC
+LIMIT 1 OFFSET 1;
+
+#OR
+
 SELECT (SELECT MAX(Salary) FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee)) SecondHighestSalary;
 # or
 SELECT (SELECT Salary FROM Employee GROUP BY Salary ORDER BY Salary DESC LIMIT 1,1) SecondHighestSalary;
